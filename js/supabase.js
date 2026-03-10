a891e6360ec641be89ccf5a9d17516ecbc589917
jamesgamemer <jamessarutj@gmail.com>
2026-03-10 21:25:41 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const supabase = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
 "https://gzlkdxigiejwwxewyikq.supabase.co",
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6bGtkeGlnaWVqd3d4ZXd5aWtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NjU2NzMsImV4cCI6MjA4ODU0MTY3M30.62lXidTteew5WB2r3kmm6zqBVHB_6AHLz-vWwj3rRrs"
 )
