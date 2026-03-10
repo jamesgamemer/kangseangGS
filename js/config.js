7c92f05967c83205206830c94ba3e5102d579061
jamesgamemer <jamessarutj@gmail.com>
2026-03-10 18:47:35 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,8 +9,7 @@ const GuideSystemConfig = {
     url: 'https://gzlkdxigiejwwxewyikq.supabase.co',
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6bGtkeGlnaWVqd3d4ZXd5aWtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NjU2NzMsImV4cCI6MjA4ODU0MTY3M30.62lXidTteew5WB2r3kmm6zqBVHB_6AHLz-vWwj3rRrs',
     bucket: 'guide-media'
-  }
-};
+  },
 
   // Media Upload Settings
   media: {
