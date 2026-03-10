2206023254537570e0ce83f47564e582f361e0ee
jamesgamemer <jamessarutj@gmail.com>
2026-03-10 18:43:07 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,8 +6,8 @@
 const GuideSystemConfig = {
   supabase: {
     enabled: true,
-    url: 'https://your-project.supabase.co',
-    key: 'your-anon-key',
+    url: 'https://gzlkdxigiejwwxewyikq.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6bGtkeGlnaWVqd3d4ZXd5aWtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NjU2NzMsImV4cCI6MjA4ODU0MTY3M30.62lXidTteew5WB2r3kmm6zqBVHB_6AHLz-vWwj3rRrs',
     bucket: 'guide-media'
   }
 };
